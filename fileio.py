08a64d1fc9bcba66bfaba179be72f0de5ba0ecff
name-88 <sasha33433@gmail.com>
2024-10-07 13:23:46 +0300

    функция сохранения

--- fileio.py
+++ fileio.py
@@ -4,6 +4,24 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
 import pyperclip
+import json
+import os
+
+
+history_file = "upload_history.json"
+
+
+def save_history(file_path, download_link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, "r") as file:
+            history = json.load(file)
+
+    history.append({"file_path": os.path.basename(file_path), "download_link": download_link})
+
+    with open(history_file, "w") as file:
+        json.dump(history, file, indent=4)
+
 
 def upload():
     try:
@@ -18,6 +36,7 @@ def upload():
                     entry.delete(0, END)
                     entry.insert(0, download_link)
                     pyperclip.copy(download_link)  # Копирование ссылки в буфер обмена
+                    save_history(filepath, download_link)
                     mb.showinfo("Ссылка скопирована", f"Ссылка {download_link} успешно скопирована")
 
                 else:
